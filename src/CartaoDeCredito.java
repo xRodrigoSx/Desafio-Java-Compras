7961d8cb01a0c722f694a7f0b08a0146b24a6e27
Rodrigo Santos <119634097+xRodrigoSx@users.noreply.github.com>
2024-07-24 12:18:38 -0300

    Add files via upload

---
+++ src/CartaoDeCredito.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public boolean lancaCompra(Compra compra) {
+        if (this.saldo > compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+}
